3295e04278d2fe32e420194a3ef07023b4542c67
Prashant Khanal <prashantkhanal32@gmail.com>
2021-05-30 15:38:42 +0545

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const showModalBtns = document.querySelector('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+
+const openModalfunc = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModalfunc = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+closeModalBtn.addEventListener('click', closeModalfunc);
+
+showModalBtns.addEventListener('click', openModalfunc);
